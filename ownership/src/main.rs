c5e78957728f4a49c96581f4533237280b7f6c8d
siddharthteli12 <telisiddharth123@yahoo.in>
2023-04-06 10:51:07 +0530

    Add: Mod_2, test_copy utility.

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     test_move();
+    test_copy();
 }
 
 // Test the concept of move.
@@ -16,4 +17,45 @@ fn test_move() {
     let string_2 = string_1;
     // Error - value borrowed here after move
     // println!("Is string_1 valid - {:?}", string_1);
+}
+
+// Cannot impl Copy trait because of string type which impl drop.
+#[derive(Debug)]
+struct Student {
+    age: i32,
+    height: i32,
+    name: String,
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Student1 {
+    age: i32,
+    height: i32,
+}
+
+// Test copy trait on stucts.
+// Note: All primitive types which are stored on stack impl Copy trait.
+// Hence, values are not moved but copied.
+// Copy trait cannot be impl for types which imppl drop trait.
+fn test_copy() {
+    let student = Student {
+        age: 23,
+        height: 170,
+        name: String::from("Siddharth"),
+    };
+
+    let student1 = Student1 {
+        age: 23,
+        height: 170,
+    };
+
+    // Try to move value to new variable.
+    let student_new = student;
+    let student_new1 = student1;
+
+    // Invalid student doesn't impl copy trait.
+    //println!("Is student valid {:?}", student);
+
+    // Valid student1 impl copy trait.
+    println!("Is student1 valid {:?}", student1);
 }
