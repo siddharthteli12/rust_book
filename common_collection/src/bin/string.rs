091af5c8c81303d1bafa75ca30b951490a0edeb2
siddharthteli12 <telisiddharth123@yahoo.in>
2023-04-15 21:42:44 +0530

    Add: Common collection, String ascci vs utf.

--- common_collection/src/bin/string.rs
+++ common_collection/src/bin/string.rs
@@ -1,3 +1,34 @@
 fn main() {
-    println!("Hello, world!");
+    // Ascii string.
+    let mut simple_string = String::from("SiddHARTH");
+    // UTF string.
+    let complex_string = String::from("Здравству");
+
+    // Confirm if strings are ascii or not.
+    println!(
+        "Is simple_string ascii - {:} \nIs complex string ascii - {:}",
+        simple_string.is_ascii(),
+        complex_string.is_ascii()
+    );
+
+    // Length of ascci & UTF string.
+    println!(
+        "Length ascii string - {:} \nLength UTF string - {:}",
+        simple_string.len(),
+        complex_string.len()
+    );
+
+    // Push UTF string to ascii string.
+    simple_string.push_str(&complex_string);
+    // Check if simple string is ascii.
+    println!("Is simple_string ascii - {:}", simple_string.is_ascii(),);
+
+    // Simple string is mix of ascci & UTF char now.
+    // Siddharth is stored as ascii taking 1 byte storage,
+    // While Здравству as UTF taking 2 bytes.
+    println!(
+        "Length ascii string - {:} \nLength UTF string - {:}",
+        simple_string.len(),
+        complex_string.len()
+    );
 }
