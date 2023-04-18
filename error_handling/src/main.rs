4805d6d60bb9f0a6b862069d86385d530607bea9
siddharthteli12 <telisiddharth123@yahoo.in>
2023-04-18 11:24:33 +0530

    Refactor: Error_handling file with closure.

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,16 +1,13 @@
 use std::{fs::File, io::ErrorKind};
 
 fn main() {
-    let test_file = match File::open("test.txt") {
-        Ok(file) => file,
-        Err(e) => match e.kind() {
-            ErrorKind::NotFound => match File::create("test.txt") {
-                Ok(file) => file,
-                Err(e) => panic!("Problem creating new file {:?}", e),
-            },
-            other_error => {
-                panic!("Problem creating new file - {:?} ", other_error);
-            }
-        },
-    };
+    let test_file = File::open("test.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("test.txt").unwrap_or_else(|error| {
+                panic!("Error creating file {:?}", error);
+            })
+        } else {
+            panic!("{:?}", error);
+        }
+    });
 }
