eafce06183df3026598623bb5ad20c073f7c600c
siddharthteli12 <telisiddharth123@yahoo.in>
2023-04-20 00:35:35 +0530

    Refactor: Guessing game, impl on struct type to restrict range.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,12 +1,32 @@
 use rand::{thread_rng, Rng};
 use std::{cmp::Ordering, io::stdin};
+
+mod guess {
+    pub struct Guess {
+        value: i32,
+    }
+
+    impl Guess {
+        pub fn new(value: i32) -> Self {
+            if value < 0 || value > 100 {
+                panic!("Value should be between 0 to 100 only");
+            }
+            Self { value }
+        }
+        pub fn value(self) -> i32 {
+            self.value
+        }
+    }
+}
+
+use guess::Guess;
+
 fn main() {
     // Gen. random number.
     let mut rng = thread_rng();
     let random_num: i32 = rng.gen_range(1..100);
 
-    let mut user_guess: i32;
-
+    let mut user_guess: Guess;
     // Take user input till guess is right.
     loop {
         // Take user input.
@@ -15,10 +35,10 @@ fn main() {
 
         user_guess = match input_string.trim().to_string().parse::<i32>() {
             Err(e) => panic!("Not valid integer `{:}", e),
-            Ok(val) => val,
+            Ok(val) => Guess::new(val),
         };
 
-        match user_guess.cmp(&random_num) {
+        match user_guess.value().cmp(&random_num) {
             Ordering::Greater => println!("Guess value too big"),
             Ordering::Less => println!("Guess value too small"),
             Ordering::Equal => {
