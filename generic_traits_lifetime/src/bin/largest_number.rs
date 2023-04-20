6c516babadd4bfd5c0627eb40ff730481e3c4a77
siddharthteli12 <telisiddharth123@yahoo.in>
2023-04-20 11:59:32 +0530

    Add: (largest_number) use generic for largest_num method.

--- generic_traits_lifetime/src/bin/largest_number.rs
+++ generic_traits_lifetime/src/bin/largest_number.rs
@@ -4,15 +4,18 @@ fn main() {
 
     let list_2 = vec![40, 1200, 50000, 12121344, 24524, 425, 0];
     println!("Largest num in list_2 -- {:}", largest_num(&list_2));
+
+    let list_3 = vec![12.12, 124.12, 124325.32, 23423_f64];
+    println!("Largest num in list_3 -- {:}", largest_num(&list_3));
 }
 
 // Find largest num in list
-fn largest_num(list: &Vec<i32>) -> i32 {
-    let mut largest = list[0];
+fn largest_num<T: std::cmp::PartialOrd>(list: &Vec<T>) -> &T {
+    let mut largest = &list[0];
 
     for num in list {
-        if *num > largest {
-            largest = *num;
+        if num > largest {
+            largest = num;
         }
     }
 
