f9a05a725915e05e14451f82c84afc87666f0618
siddharthteli12 <telisiddharth123@yahoo.in>
2023-04-20 12:17:38 +0530

    Add: (largest number) char list.

--- generic_traits_lifetime/src/bin/largest_number.rs
+++ generic_traits_lifetime/src/bin/largest_number.rs
@@ -5,8 +5,13 @@ fn main() {
     let list_2 = vec![40, 1200, 50000, 12121344, 24524, 425, 0];
     println!("Largest num in list_2 -- {:}", largest_num(&list_2));
 
+    // Float list.
     let list_3 = vec![12.12, 124.12, 124325.32, 23423_f64];
     println!("Largest num in list_3 -- {:}", largest_num(&list_3));
+
+    // Char list
+    let list_4 = vec!['a', 'b', 'd', 'a', '1', '5', '%'];
+    println!("Largest num in list_4 -- {:}", largest_num(&list_4));
 }
 
 // Find largest num in list
