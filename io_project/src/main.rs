8d93734d0cbd57adb5e096522c83559ead004e72
siddharthteli12 <telisiddharth123@yahoo.in>
2023-05-05 22:26:01 +0530

    Refactor: (io_project) read cli arguments utility for reading cli arguments not user input.

--- io_project/src/main.rs
+++ io_project/src/main.rs
@@ -1,21 +1,39 @@
 use io_project::handle_pattern_matching;
-use std::io;
+use std::cmp::Ordering;
+use std::env;
+
+const ARGS_LEN: usize = 3;
 
 fn main() {
-    let mut input_data = String::new();
-    io::stdin()
-        .read_line(&mut input_data)
-        .expect("Issue reading input");
-    let (pattern, file_path) = input_data.rsplit_once(" ").unwrap();
-
-    match handle_pattern_matching(pattern.trim(), file_path.trim()) {
-        Ok(result_vec) => {
-            for result in result_vec {
-                println!("{:} {:}", result.0, result.1);
+    match read_cli_arguments() {
+        Ok((pattern, file_path)) => match handle_pattern_matching(&pattern, &file_path) {
+            Ok(result_vec) => {
+                for result in result_vec {
+                    println!("{:} {:}", result.0, result.1);
+                }
             }
-        }
+            Err(e) => {
+                println!("Issue in matching pattern due to {:?}", e);
+            }
+        },
         Err(e) => {
-            println!("Issue in matching pattern due to {:?}", e);
+            println!("Issue with cli arguments due to {:?}", e);
         }
+    }
+}
+
+fn read_cli_arguments() -> Result<(String, String), std::io::Error> {
+    let args: Vec<String> = env::args().collect();
+
+    match args.len().cmp(&ARGS_LEN) {
+        Ordering::Less => Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!("Expected {:} arg, found {:} arg", ARGS_LEN, args.len()),
+        )),
+        Ordering::Greater => Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!("Expected {:} arg, found {:} arg", ARGS_LEN, args.len()),
+        )),
+        Ordering::Equal => Ok((String::from(args[1].clone()), String::from(args[2].clone()))),
     }
 }
