d814a7fafc9eb4ce84f24af581eef5a0ee9a4029
siddharthteli12 <telisiddharth123@yahoo.in>
2023-05-07 21:58:56 +0530

    Add: (io_projects) Comments.

--- io_project/src/main.rs
+++ io_project/src/main.rs
@@ -2,14 +2,17 @@ use io_project::handle_pattern_matching;
 use std::cmp::Ordering;
 use std::env;
 
+// Stores args length for pattern matching in file.
 const ARGS_LEN: usize = 3;
 
+// To store pattern matching config.
 pub struct Config {
     pub pattern: String,
     pub file_path: String,
 }
 
 impl Config {
+    // Calls validate & then creates config instance.
     pub fn new(args: &[String]) -> Self {
         Self::validate_config_args(args);
         Self {
@@ -17,6 +20,8 @@ impl Config {
             file_path: args[2].clone(),
         }
     }
+
+    // Validates arg length is correct or else panics.
     pub fn validate_config_args(args: &[String]) {
         match args.len().cmp(&ARGS_LEN) {
             Ordering::Equal => return,
@@ -27,6 +32,7 @@ impl Config {
 
 fn main() {
     let config = Config::new(&env::args().collect::<Vec<String>>());
+    // Matches given pattern in given file.
     match handle_pattern_matching(&config.pattern, &config.file_path) {
         Ok(result_vec) => {
             for result in result_vec {
