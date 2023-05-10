679b052cc71fddc047332d503106e9681ff39fc8
siddharthteli12 <telisiddharth123@yahoo.in>
2023-05-10 10:54:38 +0530

    Add: (functional language) closures, impl unwarp & add test case.

--- funtional_language/src/bin/closures.rs
+++ funtional_language/src/bin/closures.rs
@@ -1,5 +1,6 @@
 mod closures_eg;
 use closures_eg::{closure_refernece, closure_thread, closure_trait, option_impl};
+
 fn main() {
     closure_refernece::reference_ownership();
     closure_thread::thread1();
--- funtional_language/src/bin/closures_eg/option_impl.rs
+++ funtional_language/src/bin/closures_eg/option_impl.rs
@@ -2,6 +2,7 @@ pub enum CustomOption<T> {
     Some1(T),
     None1,
 }
+
 use CustomOption::{None1, Some1};
 impl<T> CustomOption<T> {
     fn unwrap_or_else<F>(self, f: F) -> T
@@ -13,11 +14,49 @@ impl<T> CustomOption<T> {
             None1 => f(),
         }
     }
+
+    fn unwrap(self) -> T {
+        match self {
+            Some1(val) => val,
+            None1 => panic!("Called unwarp on none value"),
+        }
+    }
 }
 
 pub fn custom_option() {
     let value1 = Some1(1);
     let value2 = None1;
+    let value3 = Some1(10);
     println!("Value1 - {:}", value1.unwrap_or_else(|| 5));
     println!("Value2 - {:}", value2.unwrap_or_else(|| 10));
+    println!("Value1 - {:}", value3.unwrap());
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+    #[test]
+    #[should_panic]
+    fn test_unwarp_on_none() {
+        let test_value: CustomOption<i32> = None1;
+        test_value.unwrap();
+    }
+
+    #[test]
+    fn test_unwarp_on_some() {
+        let test_value: CustomOption<i32> = Some1(10);
+        assert_eq!(test_value.unwrap(), 10);
+    }
+
+    #[test]
+    fn test_unwarp_else_on_none() {
+        let test_value: CustomOption<i32> = None1;
+        assert_eq!(test_value.unwrap_or_else(|| 500), 500);
+    }
+
+    #[test]
+    fn test_unwarp_else_on_some() {
+        let test_value: CustomOption<i32> = Some1(123);
+        assert_eq!(test_value.unwrap_or_else(|| 500), 123);
+    }
 }
