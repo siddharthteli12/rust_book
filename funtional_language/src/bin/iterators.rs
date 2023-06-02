81d5e2f43c75e7f450966e4ae46112ee1c10db06
siddharthteli12 <telisiddharth123@yahoo.in>
2023-06-02 23:06:11 +0530

    Add: Iterators, test cases.

--- funtional_language/src/bin/iterators.rs
+++ funtional_language/src/bin/iterators.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code)]
+#[derive(Debug, PartialEq, Clone)]
 struct Shoe {
     id: u128,
     size: u8,
@@ -6,6 +7,9 @@ struct Shoe {
 }
 
 impl Shoe {
+    fn new(id: u128, size: u8, style: ShoeStyle) -> Self {
+        Self { id, size, style }
+    }
     fn get_size(shoe_collection: Vec<Shoe>, size: u8) -> Vec<Self> {
         shoe_collection
             .into_iter()
@@ -21,7 +25,7 @@ impl Shoe {
     }
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug, Clone)]
 enum ShoeStyle {
     Loafer,
     Sneakers,
@@ -30,3 +34,34 @@ enum ShoeStyle {
 }
 
 fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_with_empty_collection() {
+        assert_eq!(Shoe::get_size(vec![], 10), vec![]);
+        assert_eq!(Shoe::get_style(vec![], ShoeStyle::Boot), vec![]);
+    }
+
+    #[test]
+    fn test_with_simple_collection() {
+        let collections = vec![
+            Shoe::new(1, 10, ShoeStyle::Boot),
+            Shoe::new(1, 11, ShoeStyle::Boot),
+            Shoe::new(1, 11, ShoeStyle::Loafer),
+        ];
+        assert_eq!(
+            Shoe::get_size(collections.clone(), 10),
+            vec![Shoe::new(1, 10, ShoeStyle::Boot)]
+        );
+        assert_eq!(
+            Shoe::get_style(collections, ShoeStyle::Boot),
+            vec![
+                Shoe::new(1, 10, ShoeStyle::Boot),
+                Shoe::new(1, 11, ShoeStyle::Boot)
+            ]
+        );
+    }
+}
