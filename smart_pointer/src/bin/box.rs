ec6ecbeec579ff7d4b0effcb784917f786f831c9
siddharthteli12 <telisiddharth123@yahoo.in>
2023-06-11 23:01:55 +0530

    Add: Box use case 1.

--- smart_pointer/src/bin/box.rs
+++ smart_pointer/src/bin/box.rs
@@ -1,5 +1,11 @@
-// Simple smart pointer eg.
+#![allow(dead_code)]
 fn main() {
+    simple_box_eg();
+    resursive_type_eg();
+}
+
+// Simple smart pointer eg.
+fn simple_box_eg() {
     // String wrapped in box.
     let box_sur = Box::new("Teli".to_string());
     // Simple string.
@@ -8,4 +14,24 @@ fn main() {
     first_name.push_str(&box_sur);
     // Appending String with Box<String> works fine.
     println!("Value of ref to box - {:}", first_name);
+}
+
+// Box use case 1.
+// Resursive type.
+#[derive(Debug)]
+enum List<T> {
+    VALUE { val: T, next: Box<List<T>> },
+    None,
+}
+
+fn resursive_type_eg() {
+    let list1 = List::VALUE {
+        val: 12,
+        next: Box::new(List::VALUE {
+            val: 13,
+            next: Box::new(List::None),
+        }),
+    };
+
+    println!("List value - {:?}", list1);
 }
