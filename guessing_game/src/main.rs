ac7f676c9496ede62e8fb9b0c6f4bfbe06493824
siddharthteli12 <telisiddharth123@yahoo.in>
2023-07-25 11:51:49 +0530

    Refactor: Create handle user function.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,30 +8,47 @@ fn main() {
     let mut rng = thread_rng();
     let random_num: i32 = rng.gen_range(1..100);
 
-    let mut user_guess: Guess;
+    // String to store user input.
+    let mut input_string = String::new();
+
     // Take user input till guess is right.
     loop {
         // Take user input.
-        let mut input_string = String::new();
         stdin().read_line(&mut input_string).unwrap();
 
-        // Construct guess value.
-        user_guess = Guess::new(
-            input_string
-                .trim()
-                .to_string()
-                .parse::<i32>()
-                .expect("Invalid input"),
-        );
+        // Break when guess is right or continue.
+        if handle_user_input(&input_string, random_num) {
+            break;
+        } else {
+            input_string.clear();
+            continue;
+        }
+    }
+}
 
-        // Match user input against value & revert.
-        match user_guess.value().cmp(&random_num) {
-            Ordering::Greater => println!("Guess value too big"),
-            Ordering::Less => println!("Guess value too small"),
-            Ordering::Equal => {
-                println!("Congratulation right guess");
-                break;
-            }
+fn handle_user_input(input_string: &str, random_num: i32) -> bool {
+    // Construct guess value.
+    let user_guess = Guess::new(
+        input_string
+            .trim()
+            .to_string()
+            .parse::<i32>()
+            .expect("Invalid input"),
+    );
+
+    // Match user input against value & revert.
+    match user_guess.value().cmp(&random_num) {
+        Ordering::Greater => {
+            println!("Guess value too big");
+            false
+        }
+        Ordering::Less => {
+            println!("Guess value too small");
+            false
+        }
+        Ordering::Equal => {
+            println!("Congratulation right guess");
+            true
         }
     }
 }
