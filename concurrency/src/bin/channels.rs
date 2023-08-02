f1a8315a292298369d6410aeab820f86e034c185
siddharthteli12 <telisiddharth123@yahoo.in>
2023-08-02 21:38:22 +0530

    Refactor: Mul sender single receiver.

--- concurrency/src/bin/channels.rs
+++ concurrency/src/bin/channels.rs
@@ -3,23 +3,36 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let (send, receive) = mpsc::channel::<f32>();
-
-    // Send value through channel.
+    let (tx1, rx) = mpsc::channel::<f32>();
+    let tx2 = tx1.clone();
+    // Send value through channel from first thread.
     thread::spawn(move || {
         let values = vec![32.23, 232.55, 80.2390, 2032.023];
 
         for value in values {
-            send.send(value * value).unwrap();
+            tx1.send(value * value).unwrap();
             thread::sleep(Duration::from_millis(500));
         }
 
         // Receiver will stop listening for new message.
-        println!("Dropping sender...");
+        println!("Dropping 1st sender...");
+    });
+
+    // Send value through channel from second thread.
+    thread::spawn(move || {
+        let values = vec![20.23, 225.53, 323.023];
+
+        for value in values {
+            tx2.send(value * value).unwrap();
+            thread::sleep(Duration::from_millis(500));
+        }
+
+        // Receiver will stop listening for new message.
+        println!("Dropping 2nd sender...");
     });
 
     // Receiver will wait for message till sender is not dropped.
-    for value in receive {
+    for value in rx {
         println!("{:}", value);
     }
     println!("End of receiver iterator because no sender exists");
