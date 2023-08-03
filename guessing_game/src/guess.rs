7f00ba2d22ea37c91431216ad9c1cf21407f6e07
siddharthteli12 <telisiddharth123@yahoo.in>
2023-08-03 22:34:14 +0530

    Refactor: use range operator.

--- guessing_game/src/guess.rs
+++ guessing_game/src/guess.rs
@@ -4,11 +4,13 @@ pub struct Guess(i32);
 
 impl Guess {
     pub fn new(value: i32) -> Self {
-        if value < 0 || value > 100 {
+        if (0..100).contains(&value) {
+            Self(value)
+        }
+        else {
             println!("Value should be between 0 to 100 only");
             exit(0x0100)
         }
-        Self(value)
     }
     pub fn value(self) -> i32 {
         self.0
