960d23829d946d9bebd23a9f83dc4a179603c631
siddharthteli12 <telisiddharth123@yahoo.in>
2023-09-28 20:15:04 +0530

    Refactor: thread for each stream.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,30 +1,36 @@
-use std::fs;
-use std::io::{BufRead, BufReader, Write};
-use std::net::{TcpListener, TcpStream};
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:9000").unwrap();
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(client_info) => {
-                handle_connection(client_info);
-            }
-            Err(e) => println!("Connection falied due to - {:}", e),
+    let listener = TcpListener::bind("127.0.0.1:8090").expect("Unable to bind");
+    for stream_result in listener.incoming() {
+        if let Ok(stream) = stream_result {
+            thread::spawn(|| {
+                handle_stream_connection(stream);
+            });
         }
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request: String = buf_reader.lines().next().unwrap().unwrap();
-
-    let (status, filename) = if request == "GET / HTTP/1.1".to_string() {
-        ("HTTP/1.1 200 OK", "./response.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "./404.html")
+// Intensive response time & request processing.
+fn handle_stream_connection(mut stream: TcpStream) {
+    let request_stream = BufReader::new(&stream);
+    let content = request_stream.lines().next().unwrap().unwrap();
+    let (status_code, file_path) = match &content[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "./success.html"),
+        _ => ("HTTP/1.1 404 Not Found", "./not_found.html"),
     };
-    let contents = fs::read_to_string(filename).unwrap();
-    let content_length = contents.len();
-    let response = format!("{status}\r\nContent-Length: {content_length}\r\n\r\n{contents}");
+    let file = fs::read_to_string(file_path).expect("Unable to open file");
+    let length = file.len();
+    let response = format!("{status_code}\r\nContent-Length: {length}\r\n\r\n{file}");
+
+    // Mimic heavy response time.
+    thread::sleep(Duration::from_secs(5));
+
+    // Respond to request.
     stream.write_all(response.as_bytes()).unwrap();
 }
